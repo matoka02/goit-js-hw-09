5f5da2741402ef37dfe4cd5d74f3cddb6a6e651e
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-06-23 14:27:45 +0300

    update 01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+"use strict";
+
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+// console.log(buttonStart.disabled);
+// console.log(buttonStop.disabled);
+buttonStop.disabled = true;
+let timerId = 0;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+buttonStart.addEventListener('click', onStart);
+
+function onStart() {
+    buttonStart.disabled = true;
+    buttonStop.disabled = false;
+
+    timerId = setInterval(()=>{
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+};
+
+buttonStop.addEventListener('click', onStop);
+
+function onStop() {
+    clearInterval(timerId);
+    buttonStart.disabled = false;
+    buttonStop.disabled = true;
+};
 
