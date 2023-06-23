2c02a1dfe5d9c8d3fb3ec7753e8be55067fd3606
matoka02 <120492521+matoka02@users.noreply.github.com>
2023-06-23 16:23:17 +0000

    Deploying to gh-pages from @ matoka02/goit-js-hw-09@bb18c74ed6c537209f6077cd2d7b9327d115e926 🚀

---
+++ 01-color-switcher.8034c8d0.js
@@ -0,0 +1,2 @@
+!function(){"use strict";var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.disabled=!0;var a=0;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,a=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.8034c8d0.js.map
